ba28badfc7c70087877141b2fcca90ce4038d036
Egor Pristavka <pristavkaegor03@gmail.com>
2022-12-12 12:32:47 +0800

    Update main.py

--- HW Networks/Распознавание и Синтез Речи (Yandex и Google)/main.py
+++ HW Networks/Распознавание и Синтез Речи (Yandex и Google)/main.py
@@ -5,9 +5,8 @@ from speechkit import Session, SpeechSynthesis
 from google_speech import Speech
 from pydub import AudioSegment
 
-FOLDER_ID = "b1gtf3dqupicap0o7l1v"
-IAM_TOKEN = "t1.9euelZrKysfPk5yRlozJmMmJjsuZk-3rnpWais-QkJ7OlZOcic6Llpaenprl8_cIUyVj-e8pcBk1_N3z90gBI2P57ylwGTX8.wJ0IJmXJKTbutP8aSjhT36vHZWT6csFnNj_LWl3Lxc7wEyc4HoKg7tGF25UxboKVUHLsPhnEamaGdIrwrUrVDw"
-
+FOLDER_ID = "USE YOUR OWN"
+IAM_TOKEN = "USE YOUR OWN"
 def google_speech_to_text(path, lang):
     """Speech to text translation using Google"""
     r = sr.Recognizer()
